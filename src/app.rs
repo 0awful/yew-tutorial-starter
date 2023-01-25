83fabcacdc2b734eabd6651ce551fe9de3149bc0
izaac wilkowski <im.izaac.seo@gmail.com>
2023-01-24 21:53:48 -0800

    conditionals in html

--- src/app.rs
+++ src/app.rs
@@ -14,7 +14,9 @@ pub fn app() -> Html {
         a: "awe".to_owned(),
         b: "123".to_owned(),
     };
+
     log!(serde_json::to_string_pretty(&vals).unwrap());
+
     // how to do a bunch of classnames. tried a few ways
     // let title = "str" + " " + "something" doesn't work
     // you end up going down a borrowing rabbit hole I can't comprehend
@@ -22,9 +24,20 @@ pub fn app() -> Html {
     // works but does expect a fixed size.
     // This appears to be the easiest way to join an arbitrary number of strings
     let title = vec!["value", "another"].join(" ");
+
+    let message = Some("I am message");
+
     html! {
-        <div class="main">
-            <h1 class={title}>{"Yolo!"}</h1>
-        </div>
+        <>
+            <div class="main">
+                <h1 class={title}>{"Yolo!"}</h1>
+            </div>
+
+            if let Some(message) = message {
+                <p>{message}</p>
+            } else {
+                <p>{"No messages"}</p>
+            }
+        </>
     }
 }
