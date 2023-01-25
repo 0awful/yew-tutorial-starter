bd738fa8a3f8400c557cff97830c558544d49b38
izaac wilkowski <im.izaac.seo@gmail.com>
2023-01-24 21:46:59 -0800

    classnames

--- src/app.rs
+++ src/app.rs
@@ -15,9 +15,16 @@ pub fn app() -> Html {
         b: "123".to_owned(),
     };
     log!(serde_json::to_string_pretty(&vals).unwrap());
+    // how to do a bunch of classnames. tried a few ways
+    // let title = "str" + " " + "something" doesn't work
+    // you end up going down a borrowing rabbit hole I can't comprehend
+    // let title = format!("{} {}", "string", "another");
+    // works but does expect a fixed size.
+    // This appears to be the easiest way to join an arbitrary number of strings
+    let title = vec!["value", "another"].join(" ");
     html! {
-        <div>
-            <h1>{"Yolo!"}</h1>
+        <div class="main">
+            <h1 class={title}>{"Yolo!"}</h1>
         </div>
     }
 }
