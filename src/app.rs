85b484ca1ecd949cbdf30b0b6bf8d6c355be25d5
izaac wilkowski <im.izaac.seo@gmail.com>
2023-01-25 22:24:48 -0800

    added stylist

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,6 @@
 use gloo::console::log;
 use serde::{Deserialize, Serialize};
+use stylist::{style, yew::styled_component};
 use yew::prelude::*;
 
 #[derive(Serialize, Deserialize)]
@@ -8,29 +9,32 @@ struct Obj {
     b: String,
 }
 
-#[function_component(App)]
+#[styled_component(App)]
 pub fn app() -> Html {
     let vals: Obj = Obj {
         a: "awe".to_owned(),
         b: "123".to_owned(),
     };
+    // note the lack of a background color here.
+    // as the body is not impacted by these styles, and can't be no matter how hard we try,
+    // we keep the background-color in index.scss (technically we moved it from index.html, but that was all me not the tutorial)
+    let stylesheet = style!(
+        r#"
+            color: white;
+        "#
+    )
+    .unwrap();
+    // tradeoffs here. one, we have more difficulty reusing styles
+    // two, we can prevent all accidental reuse
+    // if we want to reuse we handle reuse like idiomatic rust
 
     log!(serde_json::to_string_pretty(&vals).unwrap());
-
-    // how to do a bunch of classnames. tried a few ways
-    // let title = "str" + " " + "something" doesn't work
-    // you end up going down a borrowing rabbit hole I can't comprehend
-    // let title = format!("{} {}", "string", "another");
-    // works but does expect a fixed size.
-    // This appears to be the easiest way to join an arbitrary number of strings
-    let title = vec!["value", "another"].join(" ");
-
     let messages: Vec<&str> = vec!["some message", "another", "and one more"];
 
     html! {
-        <>
-            <div class="main">
-                <h1 class={title}>{"Yolo!"}</h1>
+        <html class={stylesheet}>
+            <div>
+                <h1>{"Yolo!"}</h1>
             </div>
 
             if messages.len() > 0 {
@@ -40,7 +44,7 @@ pub fn app() -> Html {
             } else {
                 <p>{"No messages"}</p>
             }
-        </>
+        </html>
     }
 }
 
