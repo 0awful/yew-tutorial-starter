3e9484a365764b600cfaa484e3734616ac043a67
izaac wilkowski <im.izaac.seo@gmail.com>
2023-01-30 23:44:53 -0800

    hey look a problem, hard to read

--- src/components/molecules/custom_form.rs
+++ src/components/molecules/custom_form.rs
@@ -5,7 +5,7 @@ use stylist::{yew::styled_component, Style};
 use yew::prelude::*;
 
 #[styled_component(CustomForm)]
-pub fn custom_form() -> Html {
+pub fn custom_div() -> Html {
     let styles: Style = Style::new(
         r#"
             * {
@@ -15,18 +15,26 @@ pub fn custom_form() -> Html {
     )
     .expect("Failed to create style");
 
+    let button_count_state = use_state(|| 0_u32);
+
     let username_state = use_state(|| "".to_owned());
     let cloned_username_state = username_state.clone();
     let username_changed = Callback::from(move |username| {
         cloned_username_state.set(username);
     });
+    let cloned_button_count_state = button_count_state.clone();
+    let button_clicked = Callback::from(move |_| {
+        let count: u32 = *cloned_button_count_state;
+        cloned_button_count_state.set(count + 1);
+    });
     html! {
         <>
-            <form class={styles}>
+            <div class={styles}>
                 <TextInput name="username" handle_onchange={username_changed} />
-                <CustomButton label="submit" />
-            </form>
+                <CustomButton label="submit" onclick={button_clicked}/>
+            </div>
             <p>{"username: "}{&*username_state}</p>
+            <p>{"count: "}{&*button_count_state}</p>
         </>
     }
 }
